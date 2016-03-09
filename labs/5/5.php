2cf836d17eb8fc3a9865acfd0bb4fa1b33e9227a
luluos <lokinosawada@csumb.edu>
2016-03-09 19:45:26 +0000

    class3-9

---
+++ labs/5/5.php
@@ -0,0 +1,20 @@
+<?php
+$host = "";
+$dbname = "";
+$username = "luluos";
+$password = "0987654321";
+
+$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+
+    </body>
+</html>
